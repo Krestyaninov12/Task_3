4bd7dac94129aaff94b0d8aec953ed6f18214d32
kreda <kreda@S-NB134>
2021-10-21 21:44:16 +0300

    Добавьте файлы проекта.

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Введите координату первой верншины прямоугольника");
+          Console.ReadLine (x,y);
+            Console.WriteLine(x, y);
+            Console.ReadKey();
+        }
+    }
+}
